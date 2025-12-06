27d44ecb004b471d47f7d141c8c547b37a41f7b7
jakelizzi <jakelizzi@gmail.com>
2025-12-06 17:15:40 +0900

    feat: add drizzle-kit command. and migration files

--- packages/backend/src/db/schema.ts
+++ packages/backend/src/db/schema.ts
@@ -46,6 +46,8 @@ export const users = pgTable("users", {
     .notNull()
     .references(() => roles.id),
 	status: smallint("status").default(USER_STATUS.TEMPORARY).notNull(),
+	name: text("name").notNull(),
+	email: text("email").notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at")
     .defaultNow()
